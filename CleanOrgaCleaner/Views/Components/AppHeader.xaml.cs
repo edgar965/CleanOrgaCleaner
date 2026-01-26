a540a35cd04cf7fffb36768f331d05a593a6b83f
edgar965 <e@edgarm.de>
2026-01-26 13:06:44 +0100

    v1.09 Build 40 - Fix remaining blocking await in AppHeader.InitializeAsync

--- CleanOrgaCleaner/Views/Components/AppHeader.xaml.cs
+++ CleanOrgaCleaner/Views/Components/AppHeader.xaml.cs
@@ -26,7 +26,7 @@ public partial class AppHeader : ContentView
     {
         ApplyTranslations();
         UpdateUserInfo();
-        await LoadWorkStatusAsync();
+        _ = LoadWorkStatusAsync();
     }
 
     public void ApplyTranslations()
